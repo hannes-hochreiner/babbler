4599a8b789032b94209768d347c7787fc8890c81
Hannes Hochreiner <hannes@hochreiner.net>
2025-06-21 15:35:16 +0200

    update nix build script

--- flake.nix
+++ flake.nix
@@ -2,52 +2,91 @@
   description = "A tool for extracting information from keepassxc";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
 
-    crane = {
-      url = "github:ipetkov/crane";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
-    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, crane, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs {
+  outputs = { self, nixpkgs, rust-overlay, ... }:
+    let
+      system = "x86_64-linux";
+
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [ (import rust-overlay) ];
+      };
+
+      rust-bin-custom = pkgs.rust-bin.stable.latest.default.override {
+        extensions = [ "rust-src" "rust-analyzer" ];
+        targets = [ "x86_64-unknown-linux-gnu" ];
+      };
+
+      babbler-cargo-toml = (builtins.fromTOML (builtins.readFile ./Cargo.toml));
+      hashes-toml = (builtins.fromTOML (builtins.readFile ./hashes.toml));
+
+      babbler-deps = derivation {
+        inherit system;
+        name = "${babbler-cargo-toml.package.name}-${hashes-toml.cargo_lock}-deps";
+        builder = "${pkgs.nushell}/bin/nu";
+        buildInputs = with pkgs; [
+          rust-bin-custom
+        ];
+        args = [ ./builder.nu "vendor" ./. ];
+
+        outputHashAlgo = "sha256";
+        outputHashMode = "recursive";
+        outputHash = hashes-toml.deps;
+      };
+
+      babbler-bin = derivation {
           inherit system;
-        };
-
-        craneLib = crane.lib.${system};
-        babbler = craneLib.buildPackage {
-          src = craneLib.cleanCargoSource ./.;
-
-          buildInputs = [
-            # Add additional build inputs here
+          name = "${babbler-cargo-toml.package.name}-v${babbler-cargo-toml.package.version}";
+          builder = "${pkgs.nushell}/bin/nu";
+          buildInputs = with pkgs; [
+            gcc_multi
+            rust-bin-custom
           ];
-        };
-      in
-      {
-        checks = {
-          inherit babbler;
-        };
+          args = [ ./builder.nu "build" ./. babbler-deps "babbler" hashes-toml.cargo_config ];
+      };
+    in {
+      packages.${system} = {
+        deps = babbler-deps;
+        bin = babbler-bin;
+        default = babbler-bin;
+      };
 
-        packages.default = babbler;
+      devShells.${system}.default = pkgs.mkShell {
+        name = "babbler";
 
-        apps.default = flake-utils.lib.mkApp {
-          drv = babbler;
-        };
+        # Inherit inputs from checks.
+        # checks = self.checks.${system}
+        # shellHook = ''
+        #   exec zellij -l zellij.kdl
+        # '';
+        shellHook = ''
+          exec nu
+        '';
+        # Additional dev-shell environment variables can be set directly
+        # MY_CUSTOM_DEVELOPMENT_VAR = "something else"
+        # Extra inputs can be added here; cargo and rustc are provided by default.
+        buildInputs = with pkgs; [
+          # zellij
+          rust-bin-custom
+        ];
+      };
+    };
 
-        devShells.default = pkgs.mkShell {
-          inputsFrom = builtins.attrValues self.checks;
-
-          # Extra inputs can be added here
-          nativeBuildInputs = with pkgs; [
-            cargo
-            rustc
-          ];
-        };
-      }
-    );
+    nixConfig = {
+      substituters = [
+        "https://cache.nixos.org"
+        "https://hannes-hochreiner.cachix.org"
+      ];
+      trusted-public-keys = [
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+        "hannes-hochreiner.cachix.org-1:+ljzSuDIM6I+FbA0mdBTSGHcKOcEZSECEtYIEcDA4Hg="
+      ];
+    };
 }
--- mod.nu
+++ mod.nu
@@ -7,6 +7,36 @@ export def build [] {
   ^cargo build
 }
 
+export def nix-build [] {
+  ^nix build
+}
+
+export def update [] {
+	let deps_info = (get-deps-info)
+
+  ^cargo update
+  {
+    "deps": ($deps_info.hash),
+		"cargo_config": ($deps_info.cargo_config)
+    "cargo_lock": (open Cargo.lock | hash sha256)
+  } | to toml | save -f hashes.toml
+  ^nix flake update
+}
+
+def get-deps-info [] {
+  let temp_path = $"/tmp/babbler_deps_(random uuid)"
+
+  mkdir $temp_path
+	let deps_info = {
+		cargo_config: (cargo vendor $temp_path)
+		hash: (nix hash path $temp_path)
+	}
+
+  rm -r $temp_path
+
+  $deps_info
+}
+
 export def intergration_test [] {
   with-env {
     BABBLER_FILENAME: "test_files/db.kdbx",
